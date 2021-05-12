e9d1822abf8964405ea0ca5f6c42545c8cf4e59a
Cyrille Tuzi <web@cyrilletuzi.com>
2021-05-12 16:20:40 +0200

    fix tests

--- src/test/suite/test-config.ts
+++ src/test/suite/test-config.ts
@@ -1,11 +1,19 @@
 import * as vscode from 'vscode';
 
+function getWorkspaceUri(workspaceName: string): vscode.Uri {
+
+    const dirnameUri = vscode.Uri.file(__dirname);
+
+    return vscode.Uri.joinPath(dirnameUri, '..', '..', '..', 'test-workspaces', workspaceName);
+
+}
+
 export const rootProjectName = 'my-app';
 export const libProjectName = 'my-lib';
 export const subAppProjectName = 'other-app';
 export const ionicCollectionName = '@ionic/angular-toolkit';
 export const materialCollectionName = '@angular/material';
 export const userComponentTypeLabel = `Custom component type`;
-export const defaultsWorkspaceFolderUri = vscode.Uri.joinPath(vscode.Uri.parse(__dirname), '..', '..', '..', 'test-workspaces', 'defaults');
-export const customizedWorkspaceFolderUri = vscode.Uri.joinPath(vscode.Uri.parse(__dirname), '..', '..', '..', 'test-workspaces', 'customized');
-export const angularEslintWorkspaceFolderUri = vscode.Uri.joinPath(vscode.Uri.parse(__dirname), '..', '..', '..', 'test-workspaces', 'angulareslint');
+export const defaultsWorkspaceFolderUri = getWorkspaceUri('defaults');
+export const customizedWorkspaceFolderUri = getWorkspaceUri('customized');
+export const angularEslintWorkspaceFolderUri = getWorkspaceUri('angulareslint');
